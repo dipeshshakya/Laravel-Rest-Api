c8a8e1f9c7e8d37860eadf54e2b4c9e30bd01c57
dipesh shakya <dipeshshakya106@gmail.com>
2017-12-16 09:13:47 +0545

    R-002 Product Deleted

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -109,5 +109,7 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         //
+       $product->delete();
+       return response('Deleted successfully');
     }
 }
