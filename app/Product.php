c3a592a0a16e357f17523ea40e039b6aa98ff48c
dipesh shakya <dipeshshakya106@gmail.com>
2017-12-16 09:02:41 +0545

    R-002 Product updated

--- app/Product.php
+++ app/Product.php
@@ -8,6 +8,9 @@ use App\Review;
 class Product extends Model
 {
     //
+    protected $fillable=[
+        'name','detail','stock','price','discount'
+        ];
     public function review(){
         return $this->hasMany(Review::class);
     }
